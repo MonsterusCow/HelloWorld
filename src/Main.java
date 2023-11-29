e836581f81ef02a56909bfd2342a30dab7618337
MonsterusCow <ryan@starkfamily.org>
2023-11-29 11:13:50 -0600

    add sout

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Sup");
         System.out.println("This a change");
+        System.out.println("WOWIE");
+
     }
 
 }
